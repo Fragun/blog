418b85c1a9c0f78301e1256eefb5213fcbf939ff
David <dethoordavid@yahoo.fr>
2023-03-07 09:03:09 +0100

    first commit

---
+++ dist/topBar.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*****************************************!*\
+  !*** ./src/assets/javascript/topBar.js ***!
+  \*****************************************/
+console.log("topBar");
+/******/ })()
+;
+//# sourceMappingURL=topBar.bundle.js.map
